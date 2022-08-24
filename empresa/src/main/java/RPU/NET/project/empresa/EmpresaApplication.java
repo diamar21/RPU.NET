1f6dba3a6118c2311dba82fbe314fd799db592c1
diamar21 <diana.marcela.martinez.g@gmail.com>
2022-08-24 15:49:15 -0500

    Update EmpresaApplication.java
    se crea la instancia empresa quedan pendientes setters y getters

--- empresa/src/main/java/RPU/NET/project/empresa/EmpresaApplication.java
+++ empresa/src/main/java/RPU/NET/project/empresa/EmpresaApplication.java
@@ -8,6 +8,21 @@ public class EmpresaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpresaApplication.class, args);
-	}
 
+		class empresa {
+			public long idEmpresa;
+			public String nombreEmpresa;
+			public String NITEmpresa;
+			public String telefonoEmpresa;
+			public String direccionEmpresa;
+
+			public empresa(long idEmpresa, String nombreEmpresa, String NITEmpresa, String telefonoEmpresa, String direccionEmpresa) {
+				this.idEmpresa = idEmpresa;
+				this.nombreEmpresa = nombreEmpresa;
+				this.NITEmpresa = NITEmpresa;
+				this.telefonoEmpresa = telefonoEmpresa;
+				this.direccionEmpresa = direccionEmpresa;
+			}
+		}
+	}
 }
