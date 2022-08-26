450fca793118247c651b9e3b8ae6eff8d8164ea8
Jorge Enrique Fuentes L <92886895+jefuentesl@users.noreply.github.com>
2022-08-26 00:00:00 -0500

    Adición Clase empleado y clase Rol

---
+++ empresa/src/main/java/RPU/NET/project/empresa/entity/Employee.java
@@ -0,0 +1,15 @@
+package RPU.NET.project.empresa.entity;
+
+public class Employee {
+    private long idEmployee;
+    private String name;
+    private String lastName;
+    private String mail;
+    private String enterprise;
+    private String Rol rol;
+    private boolean state;
+
+
+
+
+}
---
+++ empresa/src/main/java/RPU/NET/project/empresa/entity/Rol.java
@@ -0,0 +1,8 @@
+package RPU.NET.project.empresa.entity;
+
+public class Rol {
+    private long idRol;
+    private String description;
+    private boolean state;
+
+}
