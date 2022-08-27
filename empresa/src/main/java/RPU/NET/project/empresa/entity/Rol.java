374502f5a3e5d31ec7ed56fae962acf39e1e6a6a
diamar21 <diana.marcela.martinez.g@gmail.com>
2022-08-26 21:32:36 -0500

    Update Rol.java
    rol

--- empresa/src/main/java/RPU/NET/project/empresa/entity/Rol.java
+++ empresa/src/main/java/RPU/NET/project/empresa/entity/Rol.java
@@ -1,8 +1,5 @@
 package RPU.NET.project.empresa.entity;
 
-public class Rol {
-    private long idRol;
-    private String description;
-    private boolean state;
-
+public enum Rol {
+    operativo, administrador
 }
