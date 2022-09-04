55f930753cf40cde48fd43ada11aecdf97a94095
diamar21 <diana.marcela.martinez.g@gmail.com>
2022-09-04 01:02:16 -0500

    Create IEmpresaRepository.java
    Se crea el repositorio de Empresa

---
+++ empresa/src/main/java/RPU/NET/project/empresa/Repositories/IEmpresaRepository.java
@@ -0,0 +1,8 @@
+package RPU.NET.project.empresa.Repositories;
+
+import RPU.NET.project.empresa.entity.Empresa;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IEmpresaRepository extends CrudRepository<Empresa, Long> {
+
+}
