7644a5931995374fd2223f995ff78ba1eec1d030
Sergio Palomino <97463655+Serampo@users.noreply.github.com>
2022-09-05 06:34:19 -0500

    Service de empresa

--- empresa/src/main/java/RPU/NET/project/empresa/entity/Empresa.java
+++ empresa/src/main/java/RPU/NET/project/empresa/entity/Empresa.java
@@ -62,5 +62,8 @@ public class Empresa {
     public void setDireccionEmpresa(String direccionEmpresa) {
         this.direccionEmpresa = direccionEmpresa;
     }
+
+
 }
+
 
---
+++ empresa/src/main/java/RPU/NET/project/empresa/service/EmpresaService.java
@@ -0,0 +1,70 @@
+package RPU.NET.project.empresa.service;
+
+import RPU.NET.project.empresa.entity.Empresa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpresaService implements IEmpresaService{
+    private int id;
+
+    @Override
+    public Empresa findById(int id_empresa) {
+        Empresa empresa = new Empresa();
+        empresa.setIdEmpresa(1);
+        empresa.setDireccionEmpresa("Calle 15 #95-11");
+        empresa.setTelefonoEmpresa("8157536");
+        empresa.setNITEmpresa("850235587-9");
+        empresa.setNombreEmpresa("Postobón");
+        return empresa;
+    }
+
+    @Override
+    public List<Empresa> findAll() {
+        List<Empresa> empresas = new ArrayList<>();
+        Empresa empresa1 = new Empresa();
+        empresa1.setNombreEmpresa("Postobón");
+        empresa1.setDireccionEmpresa("Calle 15 #95-11");
+        empresa1.setIdEmpresa(1);
+        empresa1.setNITEmpresa("850235587-9");
+        empresa1.setTelefonoEmpresa("8157536");
+        empresas.add(empresa1);
+        Empresa empresa2 = new Empresa();
+        empresa2.setNombreEmpresa("CocaCola");
+        empresa2.setDireccionEmpresa("Calle 45 #25-51");
+        empresa2.setIdEmpresa(2);
+        empresa2.setNITEmpresa("420690333-1");
+        empresa2.setTelefonoEmpresa("7986452");
+        empresas.add(empresa2);
+        return empresas;
+
+    }
+
+    @Override
+    public Empresa createEmpresa(Empresa empresa) {
+        Empresa newEmpresa= new Empresa();
+        newEmpresa.setNombreEmpresa(empresa.getNombreEmpresa());
+        newEmpresa.setTelefonoEmpresa(empresa.getTelefonoEmpresa());
+        newEmpresa.setDireccionEmpresa(empresa.getDireccionEmpresa());
+        newEmpresa.setNITEmpresa(empresa.getNITEmpresa());
+        newEmpresa.setIdEmpresa(empresa.getIdEmpresa());
+        return newEmpresa;
+    }
+
+    @Override
+    public Empresa updateEmpresa(Empresa empresa) {
+        Empresa updateEmpresa= findById(id);
+        updateEmpresa.setNombreEmpresa(empresa.getNombreEmpresa());
+        updateEmpresa.setTelefonoEmpresa(empresa.getTelefonoEmpresa());
+        updateEmpresa.setDireccionEmpresa(empresa.getDireccionEmpresa());
+        updateEmpresa.setNITEmpresa(empresa.getNITEmpresa());
+        updateEmpresa.setIdEmpresa(empresa.getIdEmpresa());
+        return updateEmpresa;
+    }
+
+
+
+
+
+
+}
