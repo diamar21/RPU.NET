14a9b702da1ce6610af6c10b003c0373964c0204
Jusecher <111102919+Jusecher@users.noreply.github.com>
2022-09-04 19:42:22 -0500

    Create IEmepladoRepository.java
    Se crea repositorio empleado

---
+++ empresa/src/main/java/RPU/NET/project/empresa/Repositories/IEmepladoRepository.java
@@ -0,0 +1,7 @@
+package RPU.NET.project.empresa.Repositories;
+
+import RPU.NET.project.empresa.entity.Empleado;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IEmepladoRepository extends CrudRepository<Empleado, Long> {
+}
