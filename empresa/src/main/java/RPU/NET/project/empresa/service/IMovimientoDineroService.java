7866d71c58392562fc7f564e2ae28017cc461a01
CamiloGalvis2022 <cfajar80@gmail.com>
2022-09-04 19:49:33 -0500

    CamiloGalvis2022 entity : Empresa , EmpresaApplication, Rol y Usuario

---
+++ empresa/src/main/java/RPU/NET/project/empresa/service/IMovimientoDineroService.java
@@ -0,0 +1,18 @@
+package RPU.NET.project.empresa.service;
+
+import RPU.NET.project.empresa.entity.MovimientoDinero;
+
+
+
+import java.util.List;
+
+public interface IMovimientoDineroService {
+    public MovimientoDinero findById(int id);
+    public List<MovimientoDinero> findByAll();
+    public MovimientoDinero createMovimientoDinero(MovimientoDinero movimientoDinero);
+    public MovimientoDinero updateMovimientoDinero(int id, MovimientoDinero movimientoDinero);
+    public void deletemovimientoDinero(long id);
+
+
+}
+}
---
+++ empresa/src/main/java/RPU/NET/project/empresa/service/MovimientoDineroService.java
@@ -0,0 +1,32 @@
+package RPU.NET.project.empresa.service;
+
+import RPU.NET.project.empresa.entity.MovimientoDinero;
+
+import java.util.List;
+
+public class MovimientoDineroService implements IMovimientoDineroService{
+    @Override
+    public MovimientoDinero findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<MovimientoDinero> findByAll() {
+        return null;
+    }
+
+    @Override
+    public MovimientoDinero createMovimientoDinero(MovimientoDinero movimientoDinero) {
+        return null;
+    }
+
+    @Override
+    public MovimientoDinero updateMovimientoDinero(int id, MovimientoDinero movimientoDinero) {
+        return null;
+    }
+
+    @Override
+    public void deletemovimientoDinero(long id) {
+
+    }
+}
