68bc15bc25d9cc9a889279c536ddb95d263188f6
CamiloGalvis2022 <cfajar80@gmail.com>
2022-09-17 17:34:56 -0500

    Creacion de la empresa Thymeleaf para el proyecto

---
+++ empresathymeleaf/src/test/java/RPU/NET/projectWEB/empresathymeleaf/EmpresathymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package RPU.NET.projectWEB.empresathymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresathymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
