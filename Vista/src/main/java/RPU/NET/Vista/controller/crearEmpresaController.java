6f25422b0e41d49946b1f30714bb0075b0ee9bd0
diamar21 <diana.marcela.martinez.g@gmail.com>
2022-09-20 19:10:02 -0500

    Create crearEmpresaController.java
    crear empresa controller

---
+++ Vista/src/main/java/RPU/NET/Vista/controller/crearEmpresaController.java
@@ -0,0 +1,14 @@
+package RPU.NET.Vista.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class crearEmpresaController {
+
+    @GetMapping("/crearEmpresa")
+    public String crearEmpresa(){
+        return "crearEmpresa";
+
+    }
+}
