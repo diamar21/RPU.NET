25a76c48c204f5a0def377ee45647a75bdff1e60
diamar21 <diana.marcela.martinez.g@gmail.com>
2022-09-20 19:11:46 -0500

    Update EmpleadoRestController.java

--- Vista/src/main/java/RPU/NET/Vista/controller/EmpleadoRestController.java
+++ Vista/src/main/java/RPU/NET/Vista/controller/EmpleadoRestController.java
@@ -10,30 +10,23 @@ import java.util.List;
 @RestController     // api rest
 @RequestMapping("/api")
 public class EmpleadoRestController {
-
     @Autowired
     private IEmpleadoService empleadoService;
-
     @GetMapping("/empleado/{id}")
     public Empleado findById(@PathVariable long id){
-
         return empleadoService.findById(id);
     }
-
     @GetMapping("/empleado")
     public List<Empleado> findByAll(){
         return empleadoService.findByAll();}
-
     @PostMapping("/empleado")
     public Empleado createEmpleado(@RequestBody Empleado empleado){
         return empleadoService.createEmpleado(empleado);
     }
-
     @PutMapping("/empleado")
     public Empleado updateEmpleado(@RequestBody Empleado empleado){
         return empleadoService.updateEmpleado(empleado);
     }
-
     @PatchMapping("/empleado/{id}")
     public Empleado updateIdEmpleado(@PathVariable long id, @RequestBody Empleado empleado) {
         return empleadoService.updateIdEmpleado(id, empleado);
@@ -42,12 +35,8 @@ public class EmpleadoRestController {
     public List<Empleado> getEmpresaById(@PathVariable  long id) {   // se agrega @PathVariable para que se pueda ingresar la variable id en @getmapping
         return empleadoService.getEmpresaById(id);
     }
-
-
     @DeleteMapping("/empleado/{id}")
     public void deleteEmpleado(@PathVariable long id) {
-
-
         empleadoService.deleteEmpleado((long) id);
 
     }
