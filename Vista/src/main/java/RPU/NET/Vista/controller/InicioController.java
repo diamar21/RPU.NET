e8a2ac61190a878bf38e1168f68a2ddde631a47d
Jusecher <111102919+Jusecher@users.noreply.github.com>
2022-09-25 00:22:27 -0500

    Update InicioController.java

--- Vista/src/main/java/RPU/NET/Vista/controller/InicioController.java
+++ Vista/src/main/java/RPU/NET/Vista/controller/InicioController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class InicioController {
+public class inicioController {
 
     @GetMapping("/")
     public String Inicio(){
