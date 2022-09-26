2cab4bb59f0f46045e746a497b2460c4da3ce78d
diamar21 <diana.marcela.martinez.g@gmail.com>
2022-09-26 16:44:00 -0500

    Update EmpleadoController.java

--- Vista/src/main/java/RPU/NET/Vista/controller/EmpleadoController.java
+++ Vista/src/main/java/RPU/NET/Vista/controller/EmpleadoController.java
@@ -1,73 +1,67 @@
 package RPU.NET.Vista.controller;
 
 import RPU.NET.Vista.entity.Empleado;
-import RPU.NET.Vista.entity.Empresa;
-import RPU.NET.Vista.entity.Rol;
 import RPU.NET.Vista.service.IEmpleadoService;
 import RPU.NET.Vista.service.IEmpresaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 
-import javax.validation.Valid;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@Controller
-public class EmpleadoController {
-    @Autowired
-    private IEmpleadoService empleadoService;
-    @Autowired
-    private IEmpresaService empresaService;
-    private final Logger LOG = Logger.getLogger(""+ EmpleadoController.class);
+    @Controller
+    public class EmpleadoController {
+        @Autowired
+        private IEmpleadoService empleadoService;
+        @Autowired
+        private IEmpresaService empresaService;
+        private final Logger LOG = Logger.getLogger(""+ EmpleadoController.class);
 
-    @GetMapping("/empleado/list")
-    public String getlistEmpleado (Model model,@PathVariable ("id") long id) {
+        @GetMapping("empleado/list")
+        public String getlistEmpleado (Model model) {
 
-        LOG.log(Level.INFO, "getListMovimientos");
-        List<Empleado> empleados=empleadoService.findByAll();
-        model.addAttribute("empleados", empleados);
+            LOG.log(Level.INFO, "getListMovimientos");
+            List<Empleado> empleados=empleadoService.findByAll();
+            model.addAttribute("empleados", empleados);
 
-        return "empleado/list";
+            return "list";
+        }
+
+        @GetMapping("list/empleado/{id}")
+        public Empleado findById(@PathVariable long id) {
+
+            return empleadoService.findById(id);
+        }
+
+        @GetMapping("empleado")
+        public List<Empleado> findByAll() {   // se agrega @PathVariable para que se pueda ingresar la variable id en @getmapping
+            return empleadoService.findByAll();
+        }
+
+        @GetMapping("empleado/{id}")
+        public List<Empleado> getEmpresaById(@PathVariable  long id) {
+
+            return empleadoService.getEmpresaById(id);
+        }
+        @PostMapping("empleado/crear")   //crea un nuevo rol con el id =3
+        public Empleado createEmpleado(@RequestBody Empleado empleado) {
+            return "empresa/crear";
+            return empleadoService.createEmpleado(empleado);
+
+        }
+        @PatchMapping("empleado/guardar")
+        public Empleado updateEmpleado(@RequestBody Empleado empleado) {
+
+
+            return empleadoService.updateEmpleado(empleado);
+        }
+        @DeleteMapping("eliminar/empleado/{id}")
+        public void deleteEmpleado(@PathVariable long id) {
+
+            empleadoService.deleteEmpleado(id);
+        }
     }
-
-
-    @GetMapping("/empleado/{id}")
-    public Empleado findById(@PathVariable long id) {
-
-        return empleadoService.findById(id);
-    }
-
-    @GetMapping("/empleado")
-    public List<Empleado> findByAll() {   // se agrega @PathVariable para que se pueda ingresar la variable id en @getmapping
-        return empleadoService.findByAll();
-    }
-
-    @GetMapping("/empleado/{id}")
-    public List<Empleado> getEmpresaById(@PathVariable  long id) {
-
-        return empleadoService.getEmpresaById(id);
-    }
-    @PostMapping("/empleado")   //crea un nuevo rol con el id =3
-    public Empleado createEmpleado(@RequestBody Empleado empleado) {
-        return empleadoService.createEmpleado(empleado);
-
-    }
-    @PatchMapping("/empleado")
-    public Empleado updateEmpleado(@RequestBody Empleado empleado) {
-
-
-        return empleadoService.updateEmpleado(empleado);
-    }
-    @DeleteMapping("/empleado/{id}")
-    public void deleteEmpleado(@PathVariable long id) {
-
-        empleadoService.deleteEmpleado(id);
-    }
-
-}
