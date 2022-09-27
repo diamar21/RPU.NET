4c0f0f9eb08a74df69f71f792b2aa9b987d156fc
diamar21 <diana.marcela.martinez.g@gmail.com>
2022-09-26 21:20:43 -0500

    Update EmpleadoController.java

--- Vista/src/main/java/RPU/NET/Vista/controller/EmpleadoController.java
+++ Vista/src/main/java/RPU/NET/Vista/controller/EmpleadoController.java
@@ -1,14 +1,18 @@
 package RPU.NET.Vista.controller;
 
 import RPU.NET.Vista.entity.Empleado;
+import RPU.NET.Vista.entity.Empresa;
 import RPU.NET.Vista.service.IEmpleadoService;
 import RPU.NET.Vista.service.IEmpresaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,47 +25,54 @@ import java.util.logging.Logger;
         private IEmpresaService empresaService;
         private final Logger LOG = Logger.getLogger(""+ EmpleadoController.class);
 
+        //Listar Empleado
         @GetMapping("empleado/list")
-        public String getlistEmpleado (Model model) {
-
-            LOG.log(Level.INFO, "getListMovimientos");
-            List<Empleado> empleados=empleadoService.findByAll();
+        public String getListEmpleado(Model model){
+            LOG.log(Level.INFO,"getListEmpleado");
+            List<Empleado> empleados = empleadoService.findByAll();
+            for (Empleado user : empleados)
+                System.out.println(user.toString());
             model.addAttribute("empleados", empleados);
-
             return "list";
-        }
-
-        @GetMapping("list/empleado/{id}")
-        public Empleado findById(@PathVariable long id) {
-
-            return empleadoService.findById(id);
-        }
-
-        @GetMapping("empleado")
-        public List<Empleado> findByAll() {   // se agrega @PathVariable para que se pueda ingresar la variable id en @getmapping
-            return empleadoService.findByAll();
-        }
-
-        @GetMapping("empleado/{id}")
-        public List<Empleado> getEmpresaById(@PathVariable  long id) {
-
-            return empleadoService.getEmpresaById(id);
-        }
-        @PostMapping("empleado/crear")   //crea un nuevo rol con el id =3
-        public Empleado createEmpleado(@RequestBody Empleado empleado) {
-            return "empresa/crear";
-            return empleadoService.createEmpleado(empleado);
 
         }
-        @PatchMapping("empleado/guardar")
-        public Empleado updateEmpleado(@RequestBody Empleado empleado) {
+        //Crear Empleado
+        @GetMapping("empleado/crear")
+        public String createempelado(Model modelo){
+            LOG.log(Level.INFO,"createempleado");
+            Empleado empleado = new Empleado();
+            modelo.addAttribute("empleado", empleado);
+            Empresa empresa = new Empresa();
+            List<Empleado> empresas= empleadoService.findByAll();
+            modelo.addAttribute("Empleado",empleado);
+            return "empleadoCrear";
 
-
-            return empleadoService.updateEmpleado(empleado);
         }
-        @DeleteMapping("eliminar/empleado/{id}")
-        public void deleteEmpleado(@PathVariable long id) {
-
-            empleadoService.deleteEmpleado(id);
+        //Guardar Empleado
+        @PostMapping("empleado/guardar")
+        public String guardarEmpleado(@Valid Empleado empleado, BindingResult error, Model modelo){
+            LOG.log(Level.INFO,"guardarEmpleado");
+            for(ObjectError e : error.getAllErrors())
+                System.out.println(e.toString());
+            if(error.hasErrors()) {
+                return "empleado/crear";
+            }
+            empleado = empleadoService.createEmpleado(empleado);
+            return "crearEmpleado";
+        }
+        //Editar Empleado
+        @RequestMapping(value = "empleado/editarEmpleado/{id}", method = RequestMethod.GET)
+        public String editEmpleado(@PathVariable("id") long id, Model modelo){
+            LOG.log(Level.INFO,"editEmpleado");
+            Empleado empleado = empleadoService.findById(id);
+            modelo.addAttribute("empleado", empleado);
+            List<Empresa> empresa=empresaService.findAll();
+            modelo.addAttribute("empresa",empresa);
+            return "empleadolist";
+        }
+        @RequestMapping(value = "empleado/eliminar/{id}", method = RequestMethod.GET)
+        public String deleteEmpleado(@PathVariable("id") long id, Model modelo) {
+            LOG.log(Level.INFO, "deleteEmpleado");
+            return "empleadolist";
         }
     }
