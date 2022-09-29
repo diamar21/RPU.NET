68ae7b7f1b43c5608f745a268560b05a4841a011
diamar21 <diana.marcela.martinez.g@gmail.com>
2022-09-28 22:03:27 -0500

    Update InicioController.java

--- Vista/src/main/java/RPU/NET/Vista/controller/InicioController.java
+++ Vista/src/main/java/RPU/NET/Vista/controller/InicioController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class InicioController {
 
 
-        @GetMapping("/")
+        @GetMapping("/Inicio")
         public String Inicio(){
             return "Inicio";
 
