0eaff094aa61f370bba52b8341f2e4dde59e2954
diamar21 <104181520+diamar21@users.noreply.github.com>
2022-09-28 23:20:15 -0500

    Delete empresathymeleaf directory

--- empresathymeleaf/src/main/java/RPU/NET/projectWEB/empresathymeleaf/entity/Rol.java
+++
@@ -1,6 +0,0 @@
-package RPU.NET.projectWEB.empresathymeleaf.entity;
-
-public enum Rol {
-    ROLE_USER, ROLE_ADMIN
-
-}
--- empresathymeleaf/src/main/java/RPU/NET/projectWEB/empresathymeleaf/repository/IEmpresaRepository.java
+++
@@ -1,9 +0,0 @@
-package RPU.NET.projectWEB.empresathymeleaf.repository;
-
-import RPU.NET.projectWEB.empresathymeleaf.entity.Empresa;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IEmpresaRepository extends CrudRepository <Empresa,Long> {
-
-
-}
